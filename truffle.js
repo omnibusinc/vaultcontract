90df4d26eabfef62eeccbe9f837653f9019ade80
Jason Steele <jason.omnibus@gmail.com>
2017-04-17 10:06:03 -0400

    couldn't get the test to run without these changes.  With this update, you can run `testrpc` in one console, and `truffle test --network=development` in another to run the tests.

--- truffle.js
+++ truffle.js
@@ -3,12 +3,17 @@ require('babel-polyfill');
 
 module.exports = {
   networks: {
-    test: {
-      host: "localhost",
-	  gasPrice: 1,
-      gas: 0xfffffff,
+    // test: {
+    //   host: "localhost",
+	  // gasPrice: 1,
+    //   gas: 0xfffffff,
+    //   port: 8545,
+    //   network_id: "*" // Match any network id
+    // }
+    development: {
+      host: 'localhost',
       port: 8545,
-      network_id: "*" // Match any network id
+      network_id: '*'
     }
   }
 };
